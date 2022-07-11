1280db0ce8c0580526e5bca4607c302eca39a63e
ksplash38 <omegajnr38@gmail.com>
2022-07-11 19:18:45 +0100

    tims-jewelry half done

---
+++ js/form.js
@@ -0,0 +1,29 @@
+var form = $("#contact");
+form.validate({
+    errorPlacement: function errorPlacement(error, element) { element.before(error); },
+    rules: {
+        photo: {
+            required: true,
+            accept: "image/*",
+        }
+    }
+});
+form.children("div").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+    onStepChanging: function (event, currentIndex, newIndex)
+    {
+        form.validate().settings.ignore = ":disabled,:hidden";
+        return form.valid();
+    },
+    onFinishing: function (event, currentIndex)
+    {
+        form.validate().settings.ignore = ":disabled";
+        return form.valid();
+    },
+    onFinished: function (event, currentIndex)
+    {
+        form.submit();
+    }
+});
